d5ebd9e086b6dcbbe9bc1ea70269df3a2ab1c3d3
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2025-11-24 18:27:18 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 //palavras da forca.
-var palavras = ["microfone","monitor","cadeira", "computador", "codigo"]
+var palavras = ["MONITOR","CADEIRA","TECLADO","MOUSE","EDITOR","PROGRAMA","MICROFONE","VARIAVEL","CONSTANTE","INTERNET","ROBO"]
 //variavel para desenhar no canvas.
 var canvas = document.getElementById("forca").getContext("2d")
 
